bfd87ae590ad91bbc2418ed4eaf9e53a4c7905c5
hh1508 <144179073+hh1508@users.noreply.github.com>
2023-10-28 10:15:52 -0400

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,1 +1,23 @@
+totalCost = int(input())
+large = 6.00
+extra large = 13.00
+print("What size? There is only large ($6.00) and extra large ($13.00)")
+if input = ("large") or ("extra large"): 
+    print("You chose (input)")
+  elif print("What size? There is only large ($6.00) and extra large ($13.00)")
+print("How many toppings? (Numbers 1-4)")
+1 = 1.00
+2 = 1.75
+3 = 2.50
+4 = 3.35
+if input <= 1 and >=4: 
+  print("You chose (input)")
+    elif print("How many toppings? (Numbers 1-4)"):
+aspirin = 1.00
+print("Would you like aspirin on the side for an additional $1.00?")
+    if input = yes or no
+    print(totalCost)
+elif print("Would you like aspirin on the side for an additional $1.00?"):
+  
 
+  
